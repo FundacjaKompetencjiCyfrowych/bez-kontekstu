f4444961f413772a76295db40e603831426bf05b
artur-IT <artur.it@example.com>
2025-08-21 12:49:44 +0200

    fix mobile home page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,29 +5,29 @@ export default function Home() {
   return (
     <div className="bg-blue-500">
       {/* Hero Section */}
-      <section className="flex-1 relative overflow-hidden">
-        <div className="flex flex-col items-end justify-start h-full z-10">
-          <h1 className="text-6xl text-white mr-3 mt-3 font-defectica">BEZ</h1>
+      <section className="relative w-full h-screen">
+        <div className="flex flex-col items-end justify-start z-10">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-white mr-2 sm:mr-3 mt-2 sm:mt-3 font-defectica">BEZ</h1>
         </div>
 
-        <div className="absolute bottom-0 transform ml-3 mb-3 left-0 z-10">
-          <h1 className="text-6xl text-white mt-3 flex flex-col font-defectica">
+        <div className="absolute bottom-0 transform ml-2 sm:ml-3 mb-2 sm:mb-3 left-0 z-10">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-white mt-2 sm:mt-3 flex flex-col font-defectica">
             <span>K</span>
             <span>O</span>
             <span>N</span>
           </h1>
-          <h1 className="text-6xl text-white font-defectica">TEKSTU</h1>
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-white font-defectica">TEKSTU</h1>
         </div>
 
         {/* Logo w tle - koło z rozmyciem */}
-        {/* <Image
+        <Image
           src="/logo.png"
           priority
           width={400}
           height={400}
           alt="Bez Kontekstu"
           className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-contain w-96 h-96 sm:w-128 sm:h-128 md:w-160 md:h-160 lg:w-192 lg:h-192"
-        /> */}
+        />
       </section>
 
       {/* MANIFEST Section */}
