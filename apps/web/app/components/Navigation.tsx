64622ce543742a9a6f1d3d912cfa739e93625e82
artur-IT <artur.it@example.com>
2025-09-23 10:30:08 +0200

    Fix: mobile menu height and home page margin / center

--- apps/web/app/components/Navigation.tsx
+++ apps/web/app/components/Navigation.tsx
@@ -103,8 +103,8 @@ export function Navigation() {
             >
               {/* Gray bottom line for menu open button */}
               <div className="flex items-center justify-center w-full relative">
-                <div className="absolute top-[-33px] left-0 right-0 flex justify-center h-[35px] border-b-1 border-gray-700"></div>
-                <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="z-100 absolute top-[-21px] flex justify-center">
+                <div className="absolute top-[18px] left-0 right-0 flex justify-center h-[35px] border-b-1 border-gray-700"></div>
+                <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="z-100 absolute top-[28px] flex justify-center">
                   <Image src={MobileMenuIcon} alt="Bez Kontekstu" className={`w-12 h-12 top-[-28px] `} />
                 </button>
               </div>
