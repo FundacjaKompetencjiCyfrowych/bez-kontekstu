76fa9e8dfb85bbb8b64c45e9ac218115c435762a
mp0lski <52083390+mp0lski@users.noreply.github.com>
2025-11-11 18:58:28 +0100

    add sounds schema

--- apps/studio/schemaTypes/singletons/sounds.ts
+++ apps/studio/schemaTypes/singletons/sounds.ts
@@ -3,8 +3,34 @@ import {definePage} from '../helpers'
 
 export default definePage({
   name: 'sounds',
-  title: 'Dźwięki',
+  title: `Beats'n Pieces`,
   fields: [
+    defineField({
+      name: 'name',
+      title: 'Tytuł',
+      type: 'string',
+      group: 'content',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      hidden: true,
+    }),
+    defineField({
+      name: 'timestamp',
+      title: 'Data',
+      type: 'datetime',
+      group: 'content',
+      initialValue: () => new Date().toISOString(),
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'cover',
+      title: 'Obraz kafelka',
+      type: 'richImage',
+      group: 'content',
+    }),
     defineField({
       name: 'trackUrls',
       title: 'Linki do dzwieków',
