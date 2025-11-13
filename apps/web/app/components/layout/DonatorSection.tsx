d8779a54fb6d750e18d5399cb6dfc6b92cbb146e
mp0lski <52083390+mp0lski@users.noreply.github.com>
2025-11-13 01:45:28 +0100

    fix: reverse copyField enable logic

--- apps/web/app/components/layout/DonatorSection.tsx
+++ apps/web/app/components/layout/DonatorSection.tsx
@@ -41,7 +41,7 @@ export function DonatorSection({ section, dictionary, imagePosition }: DonatorSe
                     ariaLabel={dictionary.copyToClipboard}
                     ariaLiveCopiedMessage={dictionary.copied}
                     copiedText={dictionary.copied + " ✓"}
-                    disabled={field.enableCopy}
+                    disabled={!field.enableCopy}
                   />
                 ))}
             </div>
